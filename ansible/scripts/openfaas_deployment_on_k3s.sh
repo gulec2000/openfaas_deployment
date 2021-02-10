4fb9a3ed582d84041b146858249a12915e40488d
serdar <gulec2000@gmail.com>
2021-02-10 22:17:51 +0100

    edited openfaas_deployment_on_k3s

--- ansible/scripts/openfaas_deployment_on_k3s.sh
+++ ansible/scripts/openfaas_deployment_on_k3s.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-./update_ubuntu.sh
-./ansible_setup.sh
-./copying_hosts.sh
-./docker_ins.sh
-./k3s_ins.sh
-./node-token.sh
-./faas-cli.sh
-./openfaas_ins.sh
-./update_ubuntu.sh
+./update_ubuntu.sh &
+wait; ./ansible_setup.sh &
+wait; ./copying_hosts.sh &
+wait; ./docker_ins.sh &
+wait; ./k3s_ins.sh &
+wait; ./node-token.sh &
+wait; ./faas-cli.sh &
+wait; ./openfaas_ins.sh &
+wait; ./update_ubuntu.sh &
