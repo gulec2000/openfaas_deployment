b9035a663ca3ce89a65910b7169f2cddbcaec1c3
serdar <gulec2000@gmail.com>
2021-02-11 14:09:58 +0100

    edited openfaas_ins.yml and openfaas_deployment_on_k3s.yml files

--- ansible/scripts/openfaas_deployment_on_k3s.sh
+++ ansible/scripts/openfaas_deployment_on_k3s.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-./update_ubuntu.sh &
-wait; ./ansible_setup.sh &
-wait; ./copying_hosts.sh &
-wait; ./docker_ins.sh &
-wait; ./k3s_ins.sh &
-wait; ./node-token.sh &
-wait; ./faas-cli.sh &
-wait; ./openfaas_ins.sh &
-wait; ./update_ubuntu.sh &
+bash /root/openfaas_deployment/ansible/scripts/update_ubuntu.sh &
+wait; bash /root/openfaas_deployment/ansible/scripts/ansible_setup.sh &
+wait; bash /root/openfaas_deployment/ansible/scripts/copying_hosts.sh &
+wait; bash /root/openfaas_deployment/ansible/scripts/docker_ins.sh &
+wait; bash /root/openfaas_deployment/ansible/scripts/k3s_ins.sh &
+wait; bash /root/openfaas_deployment/ansible/scripts/node-token.sh &
+wait; bash /root/openfaas_deployment/ansible/scripts/faas-cli.sh &
+wait; bash /root/openfaas_deployment/ansible/scripts/openfaas_ins.sh &
+wait; bash /root/openfaas_deployment/ansible/scripts/update_ubuntu.sh &
+wait; echo "export OPENFAAS_URL=http://159.69.209.165:31112" >> .bashrc
